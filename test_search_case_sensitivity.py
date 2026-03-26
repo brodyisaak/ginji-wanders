fcbaff5909ee6a65c185e2f2737245634e60f8ed
brodyisaak <69028457+brodyisaak@users.noreply.github.com>
2026-03-26 14:03:33 -0700

    remove broken stray search test

--- test_search_case_sensitivity.py
+++
@@ -1,4 +0,0 @@
-def test_search_case_sensitivity():
-    assert search_files('test_text', '.') == expected_output
-    assert search_files('Test_Text', '.', case_sensitive=False) == expected_output
-    assert search_files('Test_Text', '.', case_sensitive=True) != expected_output
